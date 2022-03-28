62c955ca19b0af35f8e1cd4f048bd72210aa0996
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2022-03-28 21:49:26 +0300

    socond pay button added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-
 import { useEffect, useState } from 'react';
-import stack from './assets/images/stack.png'
+import stack from './assets/images/stack.png';
 import './App.css';
 import { useHistory } from 'react-router';
 import {
-  clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey,
-} from "@solana/web3.js";
-import {
-  Link,
-} from "react-router-dom";
+  clusterApiUrl,
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from '@solana/web3.js';
+import { Link } from 'react-router-dom';
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 import { transferCustomToken } from './utils/transferToken';
-const NETWORK = clusterApiUrl("devnet");
-let lamportsRequiredToPlay = 0.1 * LAMPORTS_PER_SOL
-const gameWalletPublicKey = new PublicKey("62AtDMhgaW1YQZCxv7hGBE7HDTU67L71vs4VQrRVBq3p")
+const NETWORK = clusterApiUrl('devnet');
+let lamportsRequiredToPlay = 0.1 * LAMPORTS_PER_SOL;
+const gameWalletPublicKey = new PublicKey(
+  '62AtDMhgaW1YQZCxv7hGBE7HDTU67L71vs4VQrRVBq3p'
+);
 
 function App() {
-
-  const [provider, setProvider] = useState()
-  const [providerPubKey, setProviderPub] = useState()
-  const [loading, setLoading] = useState(false)
-  const history = useHistory()
+  const [provider, setProvider] = useState();
+  const [providerPubKey, setProviderPub] = useState();
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   /**
-   * 
+   *
    * Connection to the Solana cluster
    */
 
@@ -37,26 +38,25 @@ function App() {
      * 2. Check the wallet has SOL in it
      * 3. If no SOL then ask him to fund the wallet first
      * 4. If required SOL present the, proceed with the transaction
-     * 
+     *
      */
 
     /**
      * Check if the user is logged in
      */
     if (!providerPubKey) {
-      alert("Ooops... Please login via wallet")
-      return
+      alert('Ooops... Please login via wallet');
+      return;
     }
 
     /**
      * Check if the user has SOL in his wallet
      */
-    const accountBalance = await connection.getBalance(providerPubKey)
-    const balanceInLamports = accountBalance ? parseInt(accountBalance) : 0
+    const accountBalance = await connection.getBalance(providerPubKey);
+    const balanceInLamports = accountBalance ? parseInt(accountBalance) : 0;
     if (balanceInLamports < lamportsRequiredToPlay) {
       // alert("Not enough balance, please fund your wallet")
-      const fundNeededToPlay =
-        lamportsRequiredToPlay - balanceInLamports;
+      const fundNeededToPlay = lamportsRequiredToPlay - balanceInLamports;
       const optionsNoBalance = {
         childrenElement: () => <div />,
         customUI: ({ onClose }) => (
@@ -92,48 +92,52 @@ function App() {
         ),
         closeOnEscape: true,
         closeOnClickOutside: true,
-        willUnmount: () => { },
-        afterClose: () => { },
-        onClickOutside: () => { },
-        onKeypressEscape: () => { },
+        willUnmount: () => {},
+        afterClose: () => {},
+        onClickOutside: () => {},
+        onKeypressEscape: () => {},
         overlayClassName: 'overlay-custom-class-name',
       };
 
       confirmAlert(optionsNoBalance);
-      return
+      return;
     }
 
     /**
      * If user has required SOL in the wallet, then deduct the amount
      */
-    setLoading(true)
-    lamportsRequiredToPlay = lamportsRequiredToPlay / LAMPORTS_PER_SOL
-    const result = await transferCustomToken(provider, connection, lamportsRequiredToPlay, providerPubKey, gameWalletPublicKey)
+    setLoading(true);
+    lamportsRequiredToPlay = lamportsRequiredToPlay / LAMPORTS_PER_SOL;
+    const result = await transferCustomToken(
+      provider,
+      connection,
+      lamportsRequiredToPlay,
+      providerPubKey,
+      gameWalletPublicKey
+    );
 
     if (!result.status) {
-      alert("Error in sending the tokens, Please try again!!!")
-      return
+      alert('Error in sending the tokens, Please try again!!!');
+      return;
     }
-
 
     /**
      * If the status is true, that means transaction got successful and we can proceed
      */
-    setLoading(false)
-    history.push('/stack')
-
-  }
+    setLoading(false);
+    history.push('/stack');
+  };
 
   const loginHandler = () => {
     if (!provider && window.solana) {
-      setProvider(window.solana)
+      setProvider(window.solana);
     } else if (!provider) {
-      console.log("No provider found")
-      return
+      console.log('No provider found');
+      return;
     } else if (provider && !provider.isConnected) {
-      provider.connect()
+      provider.connect();
     }
-  }
+  };
 
   /**
    * React will call this useEffect everytime there is update in the provider variable.
@@ -144,13 +148,12 @@ function App() {
 
   useEffect(() => {
     if (provider) {
-      provider.on("connect", async () => {
-        console.log("wallet got connected", provider.publicKey)
-        setProviderPub(provider.publicKey)
-
+      provider.on('connect', async () => {
+        console.log('wallet got connected', provider.publicKey);
+        setProviderPub(provider.publicKey);
       });
-      provider.on("disconnect", () => {
-        console.log("Disconnected from wallet");
+      provider.on('disconnect', () => {
+        console.log('Disconnected from wallet');
       });
     }
   }, [provider]);
@@ -158,15 +161,36 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <Link to="/"> <h2 className="gameHeader">STACK GAME</h2> </Link>
+        <Link to="/">
+          {' '}
+          <h2 className="gameHeader">STACK GAME</h2>{' '}
+        </Link>
 
-        {!providerPubKey && <button className="loginButton" onClick={() => loginHandler()}> Login</button>}
+        {!providerPubKey && (
+          <button className="loginButton" onClick={() => loginHandler()}>
+            {' '}
+            Login
+          </button>
+        )}
         {providerPubKey && <span>{providerPubKey.toBase58()}</span>}
       </header>
       <div className="gameThumbnail">
         <div className="amountNeed">SOL needed to play: 0.1 SOL </div>
         <img src={stack} alt="Stack Game" />
-        <button className="playButton" onClick={() => playStack()}>{loading ? "Transferring SOL ..." : "Play Stack It"}</button>
+        <button className="playButton" onClick={() => playStack()}>
+          {loading ? 'Transferring SOL ...' : 'Play Stack It'}
+        </button>
+
+        <button
+          className="playButton"
+          onClick={() => {
+            //
+            // TODO: here is a onClick handler function.
+            //
+          }}
+        >
+          Test button
+        </button>
       </div>
     </div>
   );

73808769dc3699dc1c4cf0897c65539731d010c4
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2022-04-08 00:30:59 +0300

    game inputs

--- src/common/components/Game/Game.jsx
+++ src/common/components/Game/Game.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import Crossword from '@jaredreisinger/react-crossword';
+import React, { useRef } from 'react';
+import {
+  CrosswordProvider,
+  CrosswordGrid,
+  DirectionClues,
+} from '@jaredreisinger/react-crossword';
 import { Box } from '@mui/material';
 import { styles } from './Game.styles';
-import { data } from 'common/static/crossword';
+// import { data } from 'common/static/crossword';
+import data from 'common/static/crossword.json';
 
 const theme = {
   textColor: '909090',
@@ -15,9 +20,40 @@ const theme = {
 };
 
 const Game = () => {
+  const gameRef = useRef();
+
+  console.log('gameRef', gameRef);
+
   return (
     <Box sx={styles.game}>
-      <Crossword theme={theme} data={data} />
+      <CrosswordProvider
+        theme={theme}
+        data={data}
+        ref={gameRef}
+        onLoadedCorrect={e => console.log(e)}
+        onCrosswordComplete={e => console.log(e)} // fires when all the answers are correct
+        onAnswerCorrect={(e, v) => console.log(e, v)} // gets fired when the answer is correct
+        // onCellChange={e => console.log(e)} //
+        onCorrect={(direction, number, answer) =>
+          console.log(direction, number, answer)
+        }
+        // useStorage={false}
+      >
+        <Box
+          sx={{
+            width: { xs: '100%', md: '50%' },
+            padding: '8px',
+            bgcolor: '#404040',
+            borderRadius: '8px',
+          }}
+        >
+          <CrosswordGrid />
+        </Box>
+        <Box sx={{ width: { xs: '100%', md: '50%' }, display: 'flex' }}>
+          <DirectionClues style={{ bgcolor: 'red' }} direction="across" />
+          <DirectionClues direction="down" />
+        </Box>
+      </CrosswordProvider>
     </Box>
   );
 };
--- src/common/components/Game/Game.styles.js
+++ src/common/components/Game/Game.styles.js
@@ -2,6 +2,7 @@ export const styles = {
   game: {
     width: '100%',
     display: 'flex',
+    flexDirection: { xs: 'column', md: 'row' },
     gap: '24px',
     // width: { md: '50%' },
   },

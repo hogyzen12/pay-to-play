dd24928cefa9ebd5983ca1fef9c297cf527c26df
hogyzen12 <billypapas12@gmail.com>
2022-04-15 00:53:07 +0100

    implement SPL token payment

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Box, Typography, Backdrop, IconButton } from '@mui/material';
 import { Close as CloseIcon } from '@mui/icons-material';
 import { useScreenshot } from 'use-react-screenshot';
 import { confirmAlert } from 'react-confirm-alert';
+
+import * as splToken from '@solana/spl-token';
+import { Token, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import {
   clusterApiUrl,
   Connection,
@@ -24,6 +27,9 @@ import {
   ModalSuccess,
 } from 'common/components';
 import { transferCustomToken } from 'common/utils/transferToken';
+import { transferDiamondToken } from './common/utils/transferDiamond';
+import './common/utils/bufferFill';
+
 import { PrivateRoute } from 'common/utils/PrivateRoute';
 import { initialAlersState } from 'common/static/alert';
 import { routes } from './routes';
@@ -31,10 +37,13 @@ import AppLayout from 'common/layout/AppLayout';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 
 let lamportsRequiredToPlay = 0.1 * LAMPORTS_PER_SOL;
+let diamondsRequiredToPlay = 1;
+const web3 = require('@solana/web3.js');
 const NETWORK = clusterApiUrl('devnet');
 const gameWalletPublicKey = new PublicKey(
-  '62AtDMhgaW1YQZCxv7hGBE7HDTU67L71vs4VQrRVBq3p',
+  'CproxWoLCk4QrCd3VJNUpo3QZf3bjEnTN1FuBcRbZYaw',
 );
+const tokenMint = new PublicKey('2bbAWEg52ojrnaG1ymVzpPxbpRub2Wr4DmTedAi9uDHS');
 
 const App = () => {
   const [provider, setProvider] = useState();
@@ -52,7 +61,7 @@ const App = () => {
    * Connection to the Solana cluster
    */
 
-  const connection = new Connection(NETWORK);
+  const connection = new web3.Connection(NETWORK, 'confirmed');
 
   /*
    * React will call this useEffect everytime there is update in the provider variable.
@@ -238,6 +247,16 @@ const App = () => {
   };
 
   const handlePayDHMT = async selectedItem => {
+    console.log('DHMT button clicked');
+    /*
+     * Flow to play the game
+     * 1. Check if the user is logged in
+     * 2. Check the wallet has a DMND in it
+     * 3. If no DMND then ask them to stake and get some
+     * 4. If Diamond present then proceed
+     * Check if the user is logged in
+     */
+
     if (!providerPubKey) {
       // alert('Ooops... Please login via wallet');
       setAlertState({
@@ -249,6 +268,131 @@ const App = () => {
       return;
     }
 
+    /*
+     * Check if the user has diamonds in their wallet
+     * And use the value to check if they can afford the game
+     */
+    const diamondAddress = await splToken.getAssociatedTokenAddress(
+      tokenMint,
+      providerPubKey,
+    );
+
+    /*
+     * Output the ATA to console to check manually
+     * TODO!!!! ADD ERROR HANDLE IF ATA NOT FOUND
+     */
+    console.log(diamondAddress.toString());
+    console.log('found ATA');
+
+    /*
+     * Address found and we pull balance succesfully here
+     * Print to console the amount to check
+     */
+    const diamondBalance = await connection.getTokenAccountBalance(
+      diamondAddress,
+    );
+
+    console.log(diamondBalance.value.amount);
+    console.log('found balance');
+
+    /*
+     * Go here and check to see they can afford with diamonds
+     */
+    if (diamondBalance.value.amount < 1) {
+      // alert("Not enough balance, please fund your wallet")
+      const optionsNoBalance = {
+        childrenElement: () => <div />,
+        customUI: ({ onClose }) => (
+          <Backdrop open={true} onClick={onClose}>
+            <Box
+              sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                maxWidth: { xs: '90%', md: '600px' },
+                width: '100%',
+                bgcolor: '#1D1D1D',
+                padding: '16px 16px 32px',
+                borderRadius: '8px',
+              }}
+            >
+              <IconButton
+                sx={{
+                  display: 'block',
+                  padding: '0',
+                  margin: '0',
+                  marginLeft: 'auto',
+                  marginBottom: '20px',
+                }}
+                onClick={onClose}
+              >
+                <CloseIcon sx={{ color: '#A2A2A2' }} />
+              </IconButton>
+              <Typography
+                sx={{
+                  mb: '16px',
+                  fontSize: '32px',
+                  textAlign: 'center',
+                }}
+                variant="h3"
+              >
+                Oops!!! You do need more SOL for gas fees
+              </Typography>
+              <Typography sx={{ textAlign: 'center' }}>
+                You need to have at least 1 DMND to play the game.
+              </Typography>
+            </Box>
+          </Backdrop>
+        ),
+        closeOnEscape: true,
+        closeOnClickOutside: true,
+        willUnmount: () => {},
+        afterClose: () => {},
+        onClickOutside: () => {},
+        onKeypressEscape: () => {},
+        overlayClassName: 'overlay-custom-class-name',
+      };
+
+      confirmAlert(optionsNoBalance);
+      return;
+    }
+
+    /*
+     * Time to get them to send us their Diamond
+     * For this we need to use the Associated token accounts
+     * We know the accs will exist as the payer has diamonds to have gotten to this stage
+     * we call our custom function here to do this
+     */
+    setLoading(true);
+
+    const result = await transferDiamondToken(
+      provider,
+      connection,
+      tokenMint,
+      providerPubKey,
+      gameWalletPublicKey,
+      diamondBalance.value.amount,
+    );
+
+    if (!result.status) {
+      setTransferTokenStatus(result.status);
+      // alert('Error in sending the tokens, Please try again!!!');
+      setAlertState({
+        open: true,
+        message: 'Error in sending the tokens, Please try again!!!',
+        severity: 'error',
+      });
+      return;
+    }
+
+    /*
+     * If the status is true, that means transaction got successful and we can proceed
+     */
+    setTransferTokenStatus(result.status);
+    setLoading(false);
+    // history.push('/stack');
+    // navigate('/crossword');
     navigate(selectedItem);
   };
 

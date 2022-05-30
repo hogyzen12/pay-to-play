09ba22824c7ab3603c1855a351984c5a14535f9f
dmsmakovskyi <c4.tm.gor@gmail.com>
2022-05-30 21:14:20 +0300

    request headers

--- src/common/api/api.js
+++ src/common/api/api.js
@@ -10,6 +10,8 @@ const instance = axios.create({
     Accept: 'application/json',
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
   },
 });
 
